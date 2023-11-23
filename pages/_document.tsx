a934d51af579673f657891f0cba2ab31c2d0e17f
Shoaib Ahmed <ashoaib540918@gmail.com>
2023-11-23 14:18:03 +0530

    add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -61,6 +61,22 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
             rel="stylesheet"
           />
+            <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=G-4F784QPB7V`}
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4F784QPB7V');
+          `,
+              }}
+            />
+          </>
         </Head>
         <body>
           <script>0</script>
