1f7e7d27454d3dcb341cda558ecb147c5bf1d24f
Shoaib Ahmed <ashoaib540918@gmail.com>
2024-02-06 11:47:23 +0530

    updated link

--- data/home.js
+++ data/home.js
@@ -29,7 +29,7 @@ export const latest = [
   },
   {
     text: 'Buying Better for the Planet: A Green Public Procurement Guidebook for Assam',
-    link: 'https://drive.google.com/drive/folders/1n0wRDmLBe3sT0Dp0I3DhrwWKySwVnyIV?usp=sharing',
+    link: 'https://drive.google.com/file/d/1dDDCdudn3qiOfBFk7D6paKHPD31N_93c/view',
   },
   {
     text: '“Breaking the Silos”-  a guide for initiating proactive dialogues in public procurement',
